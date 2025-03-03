1031d591dd38bb124e9d3f163c29b5bb97268651
utkishu <152878833+utkishu@users.noreply.github.com>
2025-03-03 12:51:43 +0000

    Initial Commit

---
+++ CU4053-SFML/Level.cpp
@@ -0,0 +1,47 @@
+#include "Level.h"
+
+Level::Level(sf::RenderWindow* hwnd, Input* in)
+{
+	window = hwnd;
+	input = in;
+
+	// initialise game objects
+	texture.loadFromFile("gfx/Mushroom.png");
+
+	testSprite.setTexture(&texture);
+	testSprite.setSize(sf::Vector2f(100, 100));
+	testSprite.setPosition(100, 100);
+
+}
+
+Level::~Level()
+{
+
+}
+
+// handle user input
+void Level::handleInput(float dt)
+{
+	// Close window on Escape pressed.
+	if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		window->close();
+	}
+
+}
+
+// Update game objects
+void Level::update(float dt)
+{
+	
+}
+
+// Render level
+void Level::render()
+{
+	beginDraw();
+
+	window->draw(testSprite);
+
+	endDraw();
+}
---
+++ CU4053-SFML/Level.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Framework/BaseLevel.h"
+#include "Framework/Input.h"
+#include "Framework/GameObject.h"
+#include <string.h>
+#include <iostream>
+
+
+class Level : BaseLevel {
+public:
+	Level(sf::RenderWindow* hwnd, Input* in);
+	~Level();
+
+	void handleInput(float dt) override;
+	void update(float dt) override;
+	void render() override;
+
+private:
+	// Level objects
+	GameObject testSprite;
+	sf::Texture texture;
+
+};
